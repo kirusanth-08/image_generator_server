d767f2ff0a1f4c836745d3c564676143fedc885a
Kirusanth <it21838170@my.sliit.lk>
2024-12-17 23:09:04 +0530

    refactor image generation logic and add axios dependency

--- controllers/imageGeneration.js
+++ controllers/imageGeneration.js
@@ -9,24 +9,30 @@ const generateVariences = async (req, res) => {
         console.log(req.body);
         const input = {
             prompt: "A headshot photo",
-            subject: request.body.subject,
+            subject: req.body.input.subject,
             output_format: "jpg",
             // output_quality: 80,
             negative_prompt: "",
             randomise_poses: true,
-            number_of_outputs: request.body.number_of_outputs,
-            number_of_images_per_pose: request.body.number_of_images_per_pose
-          }
+            number_of_outputs: req.body.input.number_of_outputs,
+            // number_of_images_per_pose: req.body.number_of_images_per_pose
+        };
 
-        const output = await replicate.run(
-            "fofr/consistent-character:9c77a3c2f884193fcee4d89645f02a0b9def9434f9e03cb98460456b831c8772", 
-            input
+        // const output = await replicate.run(
+        //     "fofr/consistent-character:9c77a3c2f884193fcee4d89645f02a0b9def9434f9e03cb98460456b831c8772", 
+        //     { input }
+        // );
+        const [output] = await replicate.run(
+            "fofr/consistent-character:9c77a3c2f884193fcee4d89645f02a0b9def9434f9e03cb98460456b831c8772",
+            {
+              input: input
+            }
           );
-
+        console.log(output); // Debugging
         res.status(200).json(output);
     } catch (e) {
         console.log(e);
-        res.status(500).send('An error occurred while processing the replicate request.');
+        res.status(500).send('An error occurred while processing the request.');
     }
 };
 
